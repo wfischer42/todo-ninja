0c71099669893ca413d77272a2675bd5071f905d
William Fischer <william.fischer42@gmail.com>
2019-04-02 14:49:36 -0600

    Worked through 10 lessons

---
+++ src/components/Navbar.vue
@@ -0,0 +1,51 @@
+<template>
+  <nav>
+    <v-toolbar app clipped-right>
+      <v-btn icon flat color="dark-gray" @click="toggleNav()">
+        <v-icon>menu</v-icon>
+      </v-btn>
+      <v-toolbar-title class="text-uppercase grey--text">
+        <span class="font-weight-light">Todo</span>
+        <span>Ninja</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn flat color="gray" class="grey--text">
+        <span>Sign Out</span>
+        <v-icon right>exit_to_app</v-icon>
+      </v-btn>
+      <v-btn icon color="dark-grey" @click="toggleNotepad()">
+        <v-icon>more_vert</v-icon>
+      </v-btn>
+    </v-toolbar>
+    <v-navigation-drawer v-model="showNav" dark app></v-navigation-drawer>
+    <v-navigation-drawer v-model="showNotepad" right app clipped z-index="-100" class="grey lighten-4"></v-navigation-drawer>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showNav: false,
+      showNotepad: false
+    };
+  },
+  methods: {
+    toggleNotepad() {
+      this.showNotepad = !this.showNotepad;
+      if (this.showNotepad) {
+        this.showNav = false;
+      }
+    },
+    toggleNav() {
+      this.showNav = !this.showNav;
+      if (this.showNav) {
+        this.showNotepad = false;
+      }
+    }
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/plugins/vuetify.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify/lib'
+import 'vuetify/src/stylus/app.styl'
+
+Vue.use(Vuetify, {
+  iconfont: 'md',
+  theme: {
+    // light_primary: "#abcbca",
+    // dark_primary: 
+  }
+})
